c096693033ce9b0653035e2465aa062d6867c62d
Joshua <joshua007.joshua@gmail.com>
2020-09-13 13:10:02 +0200

    Add project files.

---
+++ Console_Racebaan_Tasker/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Console_Racebaan_Tasker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
