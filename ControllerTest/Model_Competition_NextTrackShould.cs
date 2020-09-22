a5b9b8ae51f87b444c9f56c0eeb607c8ddb70757
Joshua <joshua007.joshua@gmail.com>
2020-09-22 09:48:13 +0200

    Unit Tests Start

---
+++ ControllerTest/Model_Competition_NextTrackShould.cs
@@ -0,0 +1,32 @@
+﻿using Model;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ControllerTest
+{
+    [TestFixture]
+    public class Model_Competition_NextTrackShould
+    {
+        private Competition _competition;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _competition = new Competition();
+        } 
+
+        [Test]
+        public void NextTrack_EmptyQueue_ReturnNull()
+        {
+            Track result = this._competition.NextTrack();
+            if (result == null)
+            {
+                Assert.IsNull(result);
+            }
+        }
+
+
+    }
+}
